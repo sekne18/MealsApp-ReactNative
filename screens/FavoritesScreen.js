68499af4a853211068d9ef9b968621cbe8fe9946
JanSekne <sekne50@gmail.com>
2022-08-15 20:21:54 +0200

    commit

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
+
+import MealsList from "../components/MealsList/MealsList";
+import { MEALS } from '../data/dummy-data';
+import { View } from "react-native-web";
+
+function FavoritesScreen() {
+   // const favoriteMealsCtx = useContext(FavoritesContext);
+   const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
+
+   const favoriteMeals = MEALS.filter((meal) => favoriteMealIds.includes(meal.id));
+
+   if (favoriteMeals.leangth === 0) {
+      return (
+         <View style={styles.rootContainer}>
+            <Text style={styles.text}>You have no favorite meals yet.</Text>
+         </View>
+      );
+   }
+
+   return <MealsList items={favoriteMeals}/>
+}
+
+export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+   rootContainer: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+   },
+   text: {
+      fontSize: 18,
+      fontWeight: "bold",
+      color: 'white',
+   },
+});
